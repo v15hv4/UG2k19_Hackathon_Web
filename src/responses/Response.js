71e30bb8718121188d3b8a11d0b0ff67d58fad4d
Mehul1604 <mathurmehul3@gmail.com>
2020-05-19 02:51:59 +0530

    Added a cat.js to the commands folder Added its import to the respons.js Added the cat() function call in response.js

--- src/responses/Response.js
+++ src/responses/Response.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ls from './commands/ls';
 import sudo from './commands/sudo';
 import clear from './commands/clear';
+import cat from './commands/cat'
 
 const Response = (command) => {
     if (command.match(/^sudo\D*/))
@@ -12,9 +13,7 @@ const Response = (command) => {
     // Waiting on Mehul
     else if (command.match(/^cat\D*/)) {
         const fileName = command.split(' ')[1];
-        return (
-            <span className='text-danger'>cat: {fileName}: No such file or directory</span>
-        )
+        return cat(filename);
     }
     else if (command === "clear")
         return clear();
