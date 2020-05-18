868e28108b0c0b8f0b4a900f2df4942e9d515f9c
Dhruv Kapur <dhruvkapur4@gmail.com>
2020-05-19 01:40:17 +0530

    Minor Changes
    Cleaned up some debuggin statements, modularized Response.js,  increased length of command text box, made the form responsive using flex-box and now, clicking anywhere on the terminal brings the textbox into focus

--- src/components/CLI.js
+++ src/components/CLI.js
@@ -9,9 +9,7 @@ class CLI extends React.Component {
         response: <span></span>
     }
     componentDidMount() {
-        document.querySelector('form .command').addEventListener('blur', () => {
-            document.querySelector('form .command').focus()
-        })
+        document.querySelector('.command').focus();
     }
     handleChange = (e) => {
         this.setState({ command: e.target.value });
@@ -19,19 +17,19 @@ class CLI extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.setState({ response: Response(this.state.command) });
-        console.log(this.state.response);
         this.setState({ command: "" })
     }
+    shiftFocus = e => document.querySelector('.command').focus();
     render() {
         return (
-            <div className='CLI jumbotron mx-0 pt-0 rounded-bottom shadow overflow-auto' style={{ height: "80vh" }}>
+            <div className='CLI jumbotron mx-0 pt-0 rounded-bottom shadow overflow-auto' onClick={this.shiftFocus} style={{ height: "80vh" }}>
                 <div className='row justify-content-center'>
                     <img src='h19_term_header.png' alt='Hackathon2k19' className="img-fluid"></img>
                 </div>
                 <div className='row pt-3'>
-                    <form onSubmit={this.handleSubmit}>
-                        faccha@hackathon2k19: {this.state.page} $
-                        <input type='text' className='command ml-2' autoFocus onChange={this.handleChange} value={this.state.command} />
+                    <form onSubmit={this.handleSubmit} className='col-12 pl-0'>
+                        <span className='col-12 col-md-4 px-0'>faccha@hackathon2k19: {this.state.page} $</span>
+                        <input type='text' className='command px-1 col-12 col-md-8' autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" onChange={this.handleChange} value={this.state.command} />
                     </form>
                 </div>
                 <div className='row'>
---
+++ src/responses/commands/clear.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const clear = () => {
+    return (<span></span>)
+};
+
+export default clear;
