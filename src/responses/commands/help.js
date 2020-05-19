a47fc8a7b3b1dafacdeb901ffa0abb98655fcfa9
Mehul1604 <mathurmehul3@gmail.com>
2020-05-19 13:33:36 +0530

    Changes in help.js

--- src/responses/commands/help.js
+++ src/responses/commands/help.js
@@ -4,22 +4,15 @@ import React from 'react'
 const help = () => {
 	return(
 			<div className='Help pr-2'>
-				<h1 className='my-1'>List Of Commands</h1>
+				<h1 className='my-1'>List Of Important Commands</h1>
 				<ol>
 					<li>cat [filename] ->  Display the contents of the file</li>
-					<li>cd [Path to Directory] -> Navigate to a directory </li>
-						<ul>
-							<li>cd .  ->  Stay in current directory</li>
-							<li>cd .. ->  Go to parent directory</li>
-							<li>cd [Directory path] ->  Go to the said directory</li>
-						</ul>
-
 					<li>clear ->  Clear the screen </li>
-					<li>help ->  See a list of Commands</li>
 					<li>ls ->  See a list of all file names</li>
-					<li>sudo ->  Unlocks super user abilities</li>
 
 				</ol>
+				<p>******************</p>
+				<p>Feel free to try out other commands you are familiar with :)</p>
 
 			</div>
 
