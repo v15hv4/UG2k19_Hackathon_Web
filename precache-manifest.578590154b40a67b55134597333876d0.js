b991e2099a4b7250c0f3ded7198b22989d72022c
v15hv4 <vishva2912@gmail.com>
2020-05-19 17:22:23 +0530

    Updates

---
+++ precache-manifest.578590154b40a67b55134597333876d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75c9a61fea7b64147f846c9cd82e7b6",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "36e4b3ee9eadf9bd2156",
+    "url": "/web/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3ceb44a10121e8e69371",
+    "url": "/web/static/css/main.b8fdacb4.chunk.css"
+  },
+  {
+    "revision": "36e4b3ee9eadf9bd2156",
+    "url": "/web/static/js/2.9c219c15.chunk.js"
+  },
+  {
+    "revision": "5c371d7d817953b9eefd54a13d199334",
+    "url": "/web/static/js/2.9c219c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ceb44a10121e8e69371",
+    "url": "/web/static/js/main.ae242e72.chunk.js"
+  },
+  {
+    "revision": "d0380a76c1ca67af4d3c",
+    "url": "/web/static/js/runtime-main.66e9f012.js"
+  }
+]);
